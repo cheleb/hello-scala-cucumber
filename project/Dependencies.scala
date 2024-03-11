1092c5ce6dd501714e50dfacfbc937129f64ca08
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-03-11 14:45:04 +0100

    :arrow_up: Scala 3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val cucumberJUnit = "7.11.1"
-    val cucumberScala = "8.14.1"
+    val cucumberJUnit = "7.15.0"
+    val cucumberScala = "8.20.0"
   }
   val munit = Seq("org.scalameta" %% "munit" % "0.7.29" % Test)
   val cucumber = Seq(
